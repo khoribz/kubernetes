cf7d6ab332b023f9d3ea734df0545e1e53339151
Khorunzhiy Ilya <khorunzhiy.ib@gmail.com>
2024-05-19 01:17:43 +0300

    HW-2

---
+++ istio/start_istio.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -L https://istio.io/downloadIstio | sh -
+cd istio-*
+export PATH=$PWD/bin:$PATH
+istioctl install --set profile=demo -y
+kubectl label namespace default istio-injection=enabled
+kubectl apply -f manifests/ban_traffic.yml
